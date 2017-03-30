f9aaaf2fd696861881ef361e4f11bff7f9593d3d
Ralf Klammer <milkbread@freenet.de>
2017-03-30 22:28:20 +0200

    Switching to agency template

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -1,77 +1,34 @@
-require(['jQuery', 'boostrap'], function($){
+require(['jQuery', 'boostrap', 'jquery.easing'], function($){
 
 	$( document ).ready(function() {
 
-		// Closes the sidebar menu
-		$("#menu-close").click(function(e) {
-		    e.preventDefault();
-		    $("#sidebar-wrapper").toggleClass("active");
+		// jQuery for page scrolling feature - requires jQuery Easing plugin
+		$('a.page-scroll').bind('click', function(event) {
+		    var $anchor = $(this);
+		    $('html, body').stop().animate({
+		        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+		    }, 1250, 'easeInOutExpo');
+		    event.preventDefault();
 		});
-		// Opens the sidebar menu
-		$("#menu-toggle").click(function(e) {
-		    e.preventDefault();
-		    $("#sidebar-wrapper").toggleClass("active");
+
+		// Highlight the top nav as scrolling occurs
+		$('body').scrollspy({
+		    target: '.navbar-fixed-top',
+		    offset: 51
 		});
-		// Scrolls to the selected menu item on the page
-		$(function() {
-		    $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
-		        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-		            var target = $(this.hash);
-		            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		            if (target.length) {
-		                $('html,body').animate({
-		                    scrollTop: target.offset().top
-		                }, 1000);
-		                return false;
-		            }
-		        }
-		    });
+		console.log($('.navbar-fixed-top'))
+
+		// Closes the Responsive Menu on Menu Item Click
+		$('.navbar-collapse ul li a').click(function(){ 
+		        $('.navbar-toggle:visible').click();
 		});
-		//#to-top button appears after scrolling
-		var fixed = false;
-		$(document).scroll(function() {
-		    if ($(this).scrollTop() > 250) {
-		        if (!fixed) {
-		            fixed = true;
-		            // $('#to-top').css({position:'fixed', display:'block'});
-		            $('#to-top').show("slow", function() {
-		                $('#to-top').css({
-		                    position: 'fixed',
-		                    display: 'block'
-		                });
-		            });
-		        }
-		    } else {
-		        if (fixed) {
-		            fixed = false;
-		            $('#to-top').hide("slow", function() {
-		                $('#to-top').css({
-		                    display: 'none'
-		                });
-		            });
-		        }
+
+		// Offset for Main Navigation
+		$('#mainNav').affix({
+		    offset: {
+		        top: 100
 		    }
-		});
-		// Disable Google Maps scrolling
-		// See http://stackoverflow.com/a/25904582/1607849
-		// Disable scroll zooming and bind back the click event
-		var onMapMouseleaveHandler = function(event) {
-		    var that = $(this);
-		    that.on('click', onMapClickHandler);
-		    that.off('mouseleave', onMapMouseleaveHandler);
-		    that.find('iframe').css("pointer-events", "none");
-		}
-		var onMapClickHandler = function(event) {
-		        var that = $(this);
-		        // Disable the click handler until the user leaves the map area
-		        that.off('click', onMapClickHandler);
-		        // Enable scrolling zoom
-		        that.find('iframe').css("pointer-events", "auto");
-		        // Handle the mouse leave event
-		        that.on('mouseleave', onMapMouseleaveHandler);
-		    }
-		    // Enable map zooming with mouse scroll when the user clicks the map
-		$('.map').on('click', onMapClickHandler);
+		})
 
 	});
 
